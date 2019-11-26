f121e180669188ad74c200c47bcbd30eb8d7ed90
Rai Izayoi <rai.izayoi@gmail.com>
2019-11-26 18:41:05 +0900

    Add migrations for create tables

---
+++ database/migrations/2019_11_26_093559_create_home_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content_1');    // 相當於 TEXT 欄位
+            $table->text('content_2');
+            $table->string('image'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home');
+    }
+}
